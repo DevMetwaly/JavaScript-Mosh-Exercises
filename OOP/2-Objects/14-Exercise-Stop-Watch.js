c4a4ea6661ca2b1a9302553c68a3fbb39b99950e
spearkiller0 <abdohamdymetwaly@gmail.com>
2018-07-12 21:56:22 +0200

    solving OOP exercises

---
+++ OOP/2-Objects/14-Exercise-Stop-Watch.js
@@ -0,0 +1,26 @@
+function Stopwatch(){
+    let working = false, duration = 0, startTime = 0;
+
+    this.start = function(){
+        if(working) throw new Error("Stopwatch already started");
+        working = true;
+        startTime = Date.now();
+    };
+
+    this.stop = function(){
+        if(!working) throw new Error("Stopwatch is not wroking");
+        working = false;
+        duration = (Date.now() - startTime)/1000;
+    };
+
+    this.reset = function(){
+        duration = 0;
+        working = false;
+    }
+    
+    Object.defineProperty(this,'duration' ,{
+        get: function(){
+            return duration;
+        }
+    }); 
+}
